ff1300ae5833db2e30c400c4e4c8a77671600a62
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-01 17:52:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all js code as newer version
+// alert (3+3) // we are using node js not browser
+
+console.log(3
+    +3);     // code redability should be high
+console.log("Nick")
+
+
+let name = "Nick"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true or false
+// null => standalone value
+// undefined => value not assigned
+
+
+// Object 
+
+console.log(typeof undefined)     // undefined
+console.log(typeof null)    // object
