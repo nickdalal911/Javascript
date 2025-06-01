5f82de8861b644342d91a5ced7e216f9bd0791ef
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-01 17:53:45 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,11 +10,10 @@ accountCity = "delhi"
 accountPassword = "hero"
 accountEmail = "nik@12gma"
 console.log(accountId)
-
 /*
 Prefer not to use var 
 because of issue in block scope and functional scope
 */
 
-
+console.log(accountPassword)
 console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
