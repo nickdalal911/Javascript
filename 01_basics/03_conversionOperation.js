8e2de42c74c9bbdef1a61f991e425f9ebd305971
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-02 14:26:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score = undefined
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abs" => NaN
+// true = 1 , false = 0
+// null = 0
+// undefined = NaN
+
+
+let isLoggedIn = " "
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true
+// 0 => false
+// "" => false
+// " " => true , "Nikhil" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(stringNumber)
+
+
+
+// ******************* Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+// console.log(2/2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+
+str1 = "Hello"
+str2 = " Nikhil"
+str3 = str1+str2
+console.log(str3)
+console.log(str1+str2)
