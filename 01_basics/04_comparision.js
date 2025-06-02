49daccc3d9971ef689f2c279516ab5bf3f0b6ff5
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-02 17:04:30 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 < 4)
+// console.log(4 >= 1)
+// console.log(2 == 4)
+// console.log(2 > 4)
+// console.log(2 != 4)
+
+// console.log("2" < 4)
+// console.log("02" > 1)
+
+
+// console.log(null > 0)
+// console.log(null == 0)           // avoid these types of comparisions
+// console.log(null >= 0)
+
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)       // avoid these types of comparisions
+// console.log(undefined < 0)
+
+
+// ===    strict comparision
+
+console.log("2" === 2)
