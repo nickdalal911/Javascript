5a6555c4927065122bed7e5027a71d42c50ae0f7
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-05 14:46:57 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+// array 
+
+const myArr = [2,4,5,6,3]
+console.log(myArr)
+
+
+const heros = ["Shaktiman", "naagraj"]
+const myArr2 = new Array(2,4,6,3,6)
+console.log(myArr[1])
+
+
+// array methods
+
+myArr.push(6)
+myArr.push(9)
+
+console.log(myArr)
