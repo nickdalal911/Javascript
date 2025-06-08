072e77ec74654321e44180b78829d69126c03f24
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-08 15:03:06 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+// console.log("N")
+// console.log("I")
+// console.log("K")
+// console.log("H")
+// console.log("I")
+// console.log("L")
+
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("K");
+    console.log("H");
+    console.log("I");
+    console.log("L");
+}
+sayMyName();
+
+function addTwoNumbers(number1, number2) {
+      console.log(number1 + number2)
+}
+addTwoNumbers();
