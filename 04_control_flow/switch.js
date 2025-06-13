01dd1276f9ffa15e3725bebb03ad53c2ff6ef0ff
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-13 10:19:51 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch case 
+
+ // format
+
+// switch(key){
+//     case value:
+
+//     break;
+
+//     default:
+
+//     break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case march")
+        break;
+}
