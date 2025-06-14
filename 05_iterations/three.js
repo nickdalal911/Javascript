7310864a6e65cac349e10738d10c1bcd794cdf1b
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-14 10:37:36 +0000

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,26 @@
+// for of
+ 
+["","",""]
+[{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for(const num of arr){
+    console.log(num)
+}
+
+const greetings = "Hello World!"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
+}
+
+
+// Maps
+
+const map = new Map()
+map.set('In', 'India')
+map.set('Fr', 'France')
+map.set('Fr', 'France')
+map.set('USA', 'United States of America')
+
+console.log(map)
