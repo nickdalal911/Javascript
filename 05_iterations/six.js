51447b81a3b76dbe7e964fdcaabc310e4639df1b
nickdalal911 <118104291+nickdalal911@users.noreply.github.com>
2025-06-17 17:59:02 +0000

    six

---
+++ 05_iterations/six.js
@@ -0,0 +1,29 @@
+// const coding =["js", "rb", "cpp","java", "python"]
+
+// const values = coding.forEach(  (item) => {          // foreach will not return any value
+//     // console.log(item)
+//     return item
+// })
+
+// console.log(values)
+
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter(  (num) => {
+//     return num > 4                       // have to write return for output wtherwise will give empty array
+// })
+// console.log(newNums)
+
+// ++++++++++++ or +++++++++++
+
+// const newNums = []
+// myNums.forEach(  (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums)
+
